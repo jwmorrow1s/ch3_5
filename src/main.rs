f0917007d746d325612eb6a302012874c4850b60
Jeff Morrow <jwmorrow1s@gmail.com>
2020-05-12 13:33:36 -0500

    cleaned up and finished tests

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
+mod temp;
+
 fn main() {
-    //changing what x means by having the reference be mutable
-    let mut x = 5;
-    println!("The value of x is {}", x);
-    x = 6;
-    println!("The value of x is {}", x);
-
-    //creating new variables of x and shadowing the previous definition
-    //  the references are themselves immutable in this instance.
-    let x = 5;
-
-    let x = x + 1;
-
-    let x = x * 2;
-
-    println!("The value of x is {}", x);
-
-    // shadowing values where the redefinition is a different type is also possible
-
-    // first, a string;
-    let spaces = "  ";
-
-    // then, shadow this definition where the new spaces variable is a usize:
-    let spaces = spaces.len();
-
-    println!("The value of spaces is {}", spaces);
+    println!("Hello World!");
 }
--- src/temp.rs
+++ src/temp.rs
@@ -1,15 +1,63 @@
-#[derive(Debug)]
+use std::cmp::PartialEq;
+
+#[derive(Debug, PartialEq)]
 struct Temperature {
     pub fahr: f64,
     pub celsius: f64,
 }
 
 impl Temperature {
+    #[allow(dead_code)]
     pub fn from_fahrenheit(fahr: f64) -> Self {
-        Temperature { fahr: fahr, celsius: ((fahr - 32.0) * (5.0/9.0)) }
+        Temperature {
+            fahr: fahr,
+            celsius: ((fahr - 32.0) * (5.0 / 9.0)),
+        }
     }
 
+    #[allow(dead_code)]
     pub fn from_celsius(celsius: f64) -> Self {
-        Temperature { fahr: (celsius * (9.0/5.0) + 32.0), celsius: celsius }
+        Temperature {
+            fahr: (celsius * (9.0 / 5.0) + 32.0),
+            celsius: celsius,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(
+            Temperature::from_fahrenheit(32.0),
+            Temperature {
+                fahr: 32.0,
+                celsius: 0.0
+            }
+        );
+        assert_eq!(
+            Temperature::from_celsius(0.0),
+            Temperature {
+                fahr: 32.0,
+                celsius: 0.0
+            }
+        );
+        assert_eq!(
+            Temperature::from_fahrenheit(104.0),
+            Temperature {
+                fahr: 104.0,
+                celsius: 40.0
+            }
+        );
+        assert_eq!(
+            Temperature::from_celsius(40.0),
+            Temperature {
+                fahr: 104.0,
+                celsius: 40.0
+            }
+        );
     }
 }
